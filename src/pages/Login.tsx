bdbd6ba10202e086117b8fe5d19bbb5e6875ac46
William Grohe <william.grohe@gmail.com>
2022-03-20 02:25:24 -0300

    Add action on submit button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Login.scss";
 
 function Login() {
+  const [emailValue, setEmailValue] = useState("");
+  const [passwordValue, setPasswordValue] = useState("");
+
+  function handleLogin(e: string, p: string) {
+    if (e === "" || p === "") {
+      alert("Preencha os campos de Login e Senha!");
+    }
+    console.log(`Recebido email: ${e}, senha ${p}`);
+  }
+
   return (
     <div className="main">
       <div className="card">
@@ -13,14 +23,20 @@ function Login() {
           id="email"
           alt="Insira seu e-mail"
           placeholder="Seu e-mail"
+          value={emailValue}
+          onChange={(v) => setEmailValue(v.target.value)}
         />
         <input
           type="password"
           name="Password"
           id="password"
           placeholder="Sua senha"
+          value={passwordValue}
+          onChange={(v) => setPasswordValue(v.target.value)}
         />
-        <button>ENTRAR</button>
+        <button onClick={() => handleLogin(emailValue, passwordValue)}>
+          ENTRAR
+        </button>
       </div>
     </div>
   );
