2cc7600265117d2a99ae3666fee346d668eeab14
William Grohe <william.grohe@gmail.com>
2022-03-20 02:23:53 -0300

    Create div/card data input

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
-function Login() {
+import "./Login.scss";
 
+function Login() {
   return (
     <div className="main">
-
+      <div className="card">
+        <h2>LOGIN</h2>
+        <input
+          type="email"
+          name="Email"
+          id="email"
+          alt="Insira seu e-mail"
+          placeholder="Seu e-mail"
+        />
+        <input
+          type="password"
+          name="Password"
+          id="password"
+          placeholder="Sua senha"
+        />
+        <button>ENTRAR</button>
+      </div>
     </div>
   );
 }
